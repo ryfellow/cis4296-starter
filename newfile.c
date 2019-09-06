b5efedba6ce0d3010d2161a990ce58a5d728a3e0
Jennifer Chao <jgracechao@gmail.com>
2019-09-05 20:14:39 -0400

    commit part 2 of the homework

--- newfile.c
+++ newfile.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-  printf("this is a file in the newbranch branch\n");
+  printf("this is a file in the newbranch branch for the cis3296 git assignment\n");
   return 0;
 }
