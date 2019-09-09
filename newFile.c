8f1a2b9df9db0386048c6c58ea65391c947ee4c9
Alexander Photis <tug48550@temple.edu>
2019-09-09 13:01:49 -0400

    added branch with file

---
+++ newFile.c
@@ -0,0 +1,13 @@
+//
+//  newFile.c
+//  
+//
+//  Created by Alec Photis on 9/9/19.
+//
+
+int main(){
+    // Comments
+    printf("This is the file for part 2");
+    printf("Making changes by adding comments");
+    // Comments
+}
