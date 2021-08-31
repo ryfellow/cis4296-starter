d65213518b1129f61c60aae11702e5765ddba717
Zachary <zacharypreston1996@protonmail.com>
2021-08-30 20:04:45 -0400

    This is the test file

---
+++ math.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf( "Hello there how are you today?\n" );
+	int answer = 5 + 5;
+	printf( "Answer is %d\n", answer );
+	return EXIT_SUCCESS;
+}
