a10775cf520dd0792442c96b30a4d892f8bc9e1b
Ryan Stanton <tuo53772@temple.edu>
2024-01-24 14:39:38 -0500

    examplechanges

---
+++ Week1Lab.java
@@ -0,0 +1,14 @@
+
+public class Week1Lab {
+
+    public static void main(String[] args){
+
+        System.out.println("hello");
+
+        int num = 5;
+
+        System.out.println( num );
+
+    }
+
+}
