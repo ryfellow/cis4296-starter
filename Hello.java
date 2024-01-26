77b20a6d5b7af53e875bd51b26e3f17e8456ea7b
Ryan Stanton <tuo53772@temple.edu>
2024-01-25 21:06:34 -0500

    hello file

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args){
+        String first = "Ryan";
+        String last = "Stanton";
+        System.out.println("My name is " + first + " " + last);
+        System.out.println(":)");
+    }
+
+}
