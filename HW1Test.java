a54334e0091783d3adcd1270af53e8b763733859
Matt Gibson <mattgibson333@gmail.com>
2024-01-25 19:03:27 -0500

    First commit

---
+++ HW1Test.java
@@ -0,0 +1,13 @@
+/*
+Matt Gibson
+1/25/2024
+CIS 3296 Sec 3
+HW1
+ */
+public class HW1Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
